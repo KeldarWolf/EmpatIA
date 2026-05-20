5c1f9190d0105a888ef3e1f96ce963ca8536f70e
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2026-05-19 23:54:27 -0400

    Update Rutina.jsx

--- src/pages/Rutina.jsx
+++ src/pages/Rutina.jsx
@@ -534,7 +534,7 @@ export default function Rutina() {
         <button
           className="back-btn"
           onClick={() =>
-            navigate("/home")
+            navigate("/user")
           }
         >
           ⬅ Volver
